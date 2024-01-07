484101c8f0ca68adfd6e19c030dda9b84df43cd5
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-01-07 16:14:58 +0530

    current weather done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import  CurrentWeather  from './components/search/current-weather/CurrentWeather';
 import './App.css';
 import Search from './components/search/search';
+import { useState } from 'react';
 
 function App() {
+  const [currentWeather,setCurrentWeather]=useState(null)
+  const [forecast,setforecast]=useState(null)
   const handleOnSearchChange = (searchData)=>{
-    console.log(searchData);
-  }
+  const [lat,lon]= searchData.value.split(" ");
+
+  const CurrentWeatherFetch= fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e672e000839281b415d889d2a4fc94ee&units=metric`)
+  const ForecastFetch= fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=e672e000839281b415d889d2a4fc94ee&units=metric`)
+ Promise.all([CurrentWeatherFetch,ForecastFetch])
+ .then(async(response)=>{
+  const weatherResponse=await response[0].json();
+  const forecastResponse=await response[1].json();
+  setCurrentWeather({city:searchData.label,...weatherResponse})
+  setforecast({city:searchData.label,...forecastResponse})
+ })
+.catch((err=>console.log(err)));
+}
+console.log(currentWeather);
+console.log(forecast);
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
-      <CurrentWeather/>
+      {currentWeather && <CurrentWeather data={currentWeather}  />}
     </div>
   );
 }
--- src/components/search/current-weather/CurrentWeather.js
+++ src/components/search/current-weather/CurrentWeather.js
@@ -1,16 +1,42 @@
-import './current-weather.css'
-import React from 'react'
+import "./current-weather.css";
+import React from "react";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
   return (
-        <div className='weather'>
-          <div className='top'>
-            <p className='city'>Nainital</p>
-            <p className='weather-description'>sunny</p>
-          </div>
-          <img alt='weather' className='weather-icon' src=''/>
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
-  )
-}
+        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
+      </div>
+      <div className="bottom">
+        <p className="temp">{Math.round(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="para-row">
+            <span className="para-label">details</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Feels Like</span>
+            <span className="para-value">{Math.round(data.main.feels_like)}°C</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Wind</span>
+            <span className="para-value">{Math.round(data.wind.speed)}m/s</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Humidity</span>
+            <span className="para-value">{Math.round(data.main.humidity)}%</span>
+          </div>
+          <div className="para-row">
+            <span className="para-label">Pressure</span>
+            <span className="para-value">{Math.round(data.main.pressure)}hPa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
