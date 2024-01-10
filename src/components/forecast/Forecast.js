cb8eb100fe6768793a837ad64cac29ee70e50ae2
Gaurav Bhatia <115914823+bhatiag41@users.noreply.github.com>
2024-01-10 18:11:50 +0530

    major changes to the logic of hour conversion
    hopefully the last logic commit

--- src/components/forecast/Forecast.js
+++ src/components/forecast/Forecast.js
@@ -10,23 +10,24 @@ import React from "react";
 const hours = [
 '00','03','06','09','12','15','18','21'
 ];
-function convertTo12Hour(hour24) {
-  const date = new Date(`2000-01-01T${hour24}:00:00`);
-  const options = { hour: 'numeric'};
-
-  // Adjust the hour to 12-hour format
+function convertTo12Hour(hour) {
+  const date = new Date(`2000-01-01T${hour}:00:00`);
+  const options = { hour: 'numeric', minute: '2-digit' };
   const hour12 = date.getHours() % 12 || 12;
 
-  return date.toLocaleTimeString('en-US', {options, hour: '2-digit' }).replace(/^(\d{1,2}):(\d{2}) (\w{2})$/, (_, h, period) => `${hour12} ${period}`);
+  return date.toLocaleTimeString('en-US', { ...options, hour: '2-digit' }).replace(/^(\d{1,2}):(\d{2}) (\w{2})$/, (_, h, m, period) => `${hour12}:${m} ${period}`);
 }
 const Forecast = ({ data }) => {
-  const currentDate = new Date();
-const dayinaweek = currentDate.getHours().toString().padStart(2, '0');
-  console.log(dayinaweek)
-  const forecastday = hours
-  .slice(hours.indexOf(dayinaweek), hours.length)
-  .concat(hours.slice(0, hours.indexOf(dayinaweek)));
-    console.log(forecastday)
+ const currentDate = new Date();
+  const currentHour = currentDate.getHours().toString().padStart(2, '0');
+
+  const stringHours = hours.map(hour => hour.toString());
+  const forecastday = stringHours
+    .slice(stringHours.indexOf(currentHour), stringHours.length)
+    .concat(stringHours.slice(0, stringHours.indexOf(currentHour)));
+
+  const convertedHours = forecastday.map(convertTo12Hour);
+    // console.log(forecastday)
   return (
     <>
                 <div className="para-row top">
@@ -43,7 +44,7 @@ const dayinaweek = currentDate.getHours().toString().padStart(2, '0');
                     className="icon-small"
                     src={`icons/${item.weather[0].icon}.png`}
                   />
-                  <label className="day">{convertTo12Hour(forecastday[idx])}</label>
+                  <label className="day">{convertedHours[idx]}</label>
                   <label className="description">
                     {item.weather[0].description}
                   </label>
